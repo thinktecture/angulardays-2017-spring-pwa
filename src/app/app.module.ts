90f06ddf03a7b4b1e00dbb6a7b53500be7084ba9
Christian Liebel <christian.liebel@thinktecture.com>
2017-03-21 00:27:23 +0100

    ex5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ServiceWorkerModule, NgServiceWorker } from '@angular/service-worker';
 
 import { AppComponent } from './app.component';
 
@@ -12,9 +13,15 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ServiceWorkerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(serviceWorker: NgServiceWorker) {
+    serviceWorker.registerForPush()
+      .subscribe(registration => console.log(JSON.stringify(registration)));
+  }
+}
